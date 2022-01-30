e34909c3b3e3ba433ca06b47c0c961b5567dd70b
Tchannikov Daniel <75910357+Daniel-Developer-p@users.noreply.github.com>
2022-01-30 22:27:22 +0300

    bruh

---
+++ README.md
@@ -0,0 +1,3 @@
+# weather-app
+Created a simple weather app on Xcode using SwiftUI, only shows one location. My first app I created on Xcode.
+Button at the bottom that shows the weather at night time for the location.
---
+++ weather-app/weather_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  weather_appApp.swift
+//  weather-app
+//
+//  Created by Daniel Tchannikov on 30/1/2022.
+//
+
+import SwiftUI
+
+@main
+struct weather_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
